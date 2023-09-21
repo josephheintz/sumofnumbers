b23fbb9b2781b6e8389d353e8e5eb4a4dc31199c
Joseph Heintz <heintzj@hawaii.edu>
2023-09-20 15:31:04 -1000

    Update sumofnumbers.js

--- sumofnumbers.js
+++ sumofnumbers.js
@@ -1,4 +1,4 @@
-const testNums = (1, 2, 3, 4);
+const testNums = [1, 2, 3, 4];
 
 function sumFor(nums) {
   let total = 0;
@@ -9,3 +9,32 @@ function sumFor(nums) {
 }
 
 console.log(sumFor(testNums));
+
+const sumWhile = (nums) => {
+  let total = 0;
+  let i = 0;
+  while (i < nums.length) {
+    total += nums[i];
+    i++;
+  }
+  return total;
+};
+
+console.log(sumWhile(testNums));
+
+function sumRecursion(nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+  return nums[0] + sumRecursion(nums.slice(1, nums.length));
+}
+
+console.log(sumRecursion(testNums));
+
+function sumTheSimpleWay(nums) {
+  return _.reduce(nums, function (sum, num) {
+    return sum + num;
+  }, 0);
+}
+
+console.log(sumTheSimpleWay(testNums));
