68e7449336e88f4deb751a5488bb4dfe874915b9
Joseph Heintz <heintzj@hawaii.edu>
2023-09-20 14:54:46 -1000

    update sumofnumbers

---
+++ sumofnumbers.js
@@ -0,0 +1,11 @@
+const testNums = (1, 2, 3, 4);
+
+function sumFor(nums) {
+  let total = 0;
+  for (const num of nums) {
+    total += num;
+  }
+  return total;
+}
+
+console.log(sumFor(testNums));
